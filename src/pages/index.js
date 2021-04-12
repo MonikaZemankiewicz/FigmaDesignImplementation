7ca4f2a04ead45a4824f3fd652c6b38ec147ee8b
Monika <monika.zemankiewicz.2015@gmail.com>
2021-04-12 05:50:59 +0200

    react helmet

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,16 @@ import Layout from "../components/Layout";
 import Hero from "../components/Hero";
 import Team from "../components/Team";
 import Contact from "../components/Contact";
+import { Helmet } from "react-helmet";
 
 export default () => {
   return (
     <Layout>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Ethworks</title>
+        <link rel="canonical" href="http://ethworks-test.netlify.app" />
+      </Helmet>
       <Hero></Hero>
       <Team></Team>
       <Contact></Contact>
